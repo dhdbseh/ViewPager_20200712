c9fe78fa03a44d0d0d9adaef761bae09e5acf865
yd27 <dhdbseh5oe@naver.com>
2020-07-12 15:37:43 +0900

    액티비티에 뷰 페이저 올려두기

--- app/src/main/java/kr/co/tjoeun/viewpager_20200712/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200712/MainActivity.kt
@@ -3,7 +3,15 @@ package kr.co.tjoeun.viewpager_20200712
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
